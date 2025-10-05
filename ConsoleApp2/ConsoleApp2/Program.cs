abb77c7134e5a2e4f52b725e1a7cd81d9382a402
zarifatb-15 <zarifatb@code.edu.az>
2025-10-05 23:24:30 +0400

    first commit

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,34 @@
+﻿//            // 1
+// int[] numbers = {  7, 50, 10 };
+// int n =50;
+// int index = -1;
+// for (int i = 0; i < numbers.Length; i++) 
+//     if (numbers[i] == n)
+// {
+//     index = i;
+//     break;
+// }
+// if (index != -1)
+//     Console.WriteLine(index);
+// else
+// {
+//     Console.WriteLine(-1);
+// }
+// //          // 2
+// // int[] numbers = { 1, 5, 3, 4, 5 };
+// // int n = 5;
+// // int count = 0;
+// // for (int i = 0; i < numbers.Length; i++)
+// // {
+// //     if (numbers[i] == n)
+// //     {
+// //         count++;
+// //     }
+// // }
+// // Console.WriteLine(count);
+//
+//       // 3
+// 3
+//
+//
+//
