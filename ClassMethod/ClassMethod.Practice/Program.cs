60555c83cfed1fc964f7e66a099183efe718b398
zarifatb-15 <zarifatb@code.edu.az>
2025-10-18 07:40:46 +0400

    Methodpractice commit

---
+++ ClassMethod/ClassMethod.Practice/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace ClassMethod.Practice;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // // Console.WriteLine("Hello, World!");
+        // int[] arr = {  2, 3, 4, 5, };
+        // Console.WriteLine(  SumOfArrayElements(arr));
+        // var result =AddElementToArray(new int[] { 33, 44 },66);
+        // foreach (var item in result)
+        // {
+        //     Console.WriteLine(item);
+        // }
+    }
+
+    static int SumOfArrayElements(int[] array)
+    {
+        int sum = 0;
+        foreach (int num in array)
+                     sum += num;
+                     return sum;
+    }
+
+    static string RemoveStringFromString(string word)
+    {
+        string newWord=string.Empty;
+        foreach (char letter in word)
+        {
+            if(letter!=' ')
+                newWord += letter;
+                
+        }
+
+        return newWord;
+    }
+
+    static int[] AddElementToArray(int[] array, int num)
+    {
+        int[] newArray = new int[array.Length + 1];
+        for (int i = 0; i < array.Length; i++)
+              newArray[i] = array[i];
+        newArray[newArray.Length-1] = num;
+        return newArray;
+    }
+}
