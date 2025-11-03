ebc92cf2a018d1f2ce332a1acb54cf40078ceb67
zarifatb-15 <zarifatb@code.edu.az>
2025-11-03 23:51:31 +0400

    added task

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+using System.Reflection;
 using System.Threading.Channels;
 
 
@@ -6,19 +8,25 @@ using System.Threading.Channels;
 //Console.WriteLine(b.IsEven());
 //int a = 10;
 //Console.WriteLine(a.IsOdd());
+// string txt = "baBAYevA";
+// Console.WriteLine(txt.ToCapitalize());
+// string word = "babayeva";
+// int[] result = word.GetValueIndexes('a');
+// Console.WriteLine($"'{word}' sozunde 'a' herfi bu indexlerdedir:");
+// Console.WriteLine(string.Join(", ", result));
 
 
 public static class ExtentionMethods
 {
 
 
-    
+    //TASK1
     public static bool IsOdd (this int number)
     {
         return number % 2 != 0;
     }
     
-                          
+   
     public static bool IsEven(this int number)
     {
         return number % 2 == 0;
@@ -35,4 +43,32 @@ public static class ExtentionMethods
         }
         return false;
     }
+
+    public static string ToCapitalize(this string text)
+    {
+        if(string.IsNullOrEmpty(text))
+            return text;
+        char first=char.ToUpper(text[0]);
+        string rest = text.Substring(1).ToLower();
+        return first + rest;
+    }
+
+    public static int[] GetValueIndexes(this string text, char c)
+    {
+        List<int> index = new List<int>();
+        for (int i = 0; i < text.Length; i++)
+        {
+            if (text[i] == c)
+                index.Add(i);
+        }
+
+        return index.ToArray();
+    }
+
+    public static string GetFirstSentence(this string sentence)
+    {
+        
+    }
+    
+    
 }
